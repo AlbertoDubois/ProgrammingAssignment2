bfc9d05385cb0c55325b6ec68a9a4216e571cd4a
Alberto Dubois <aduboisr@gmail.com>
2014-04-25 21:52:50 +0200

    Updated Prog Assignment
    Minor comments edits

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,10 @@
-# makeCacheMatrix creates a special matrix that can cache its inverse. cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.#
+# makeCacheMatrix creates a special matrix that can cache its inverse. cacheSolve computes 
+#the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#If the inverse has already been calculated (and the matrix has not changed), 
+#then cacheSolve should retrieve the inverse from the cache.#
 
-## This function takes an argument of type numeric matrix and returns a list with 4 list items (4 functions wrapped in a list)
+## This function takes an argument of type numeric matrix and returns a list 
+#with 4 list items (4 functions wrapped in a list)
 
 makeCacheMatrix <- function(x = numeric()) {
   
